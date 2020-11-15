3c09d20db595bccfae987d58d01f94cdd6356640
Rijoanul Hasan Shanto <rijoanul.shanto@gmail.com>
2020-11-15 22:37:30 +0600

    [UPDATE] HOTFIX
    debug updated with boolean value

--- authentication/managers.py
+++ authentication/managers.py
@@ -6,11 +6,11 @@ from django.utils.translation import ugettext_lazy as _
 class CustomUserManager(BaseUserManager):
     # set email as unique identifier instead of username
 
-    def create_user(self, email, password, first_name, last_name):
+    def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError(_('The Email must be set'))
         email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, last_name=last_name)
+        user = self.model(email=email, **extra_fields)
 
         user.set_password(password)
         user.save()
