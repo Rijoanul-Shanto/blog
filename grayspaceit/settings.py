2733be5f319d98352d18d15f0f595e8c621ade91
Rijoanul Hasan Shanto <rijoanul.shanto@gmail.com>
2020-11-15 22:10:08 +0600

    [UPDATE] DEPLOY ON HEROKU
    allowed host added
    
    Resolves: #14

--- grayspaceit/settings.py
+++ grayspaceit/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['https://my-example-blog.herokuapp.com/']
+ALLOWED_HOSTS = ['my-example-blog.herokuapp.com']
 
 # Application definition
 
