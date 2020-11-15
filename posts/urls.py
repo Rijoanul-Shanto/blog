0dfb9a2a3fa152e33c42ef3010474dd0a8f525c8
Rijoanul Hasan Shanto <rijoanul.shanto@gmail.com>
2020-11-15 15:47:30 +0600

    [ADD] Integrate JSON Placeholder
    Json placeholder integrated
        - got posts from JSON placeholder
    
    Resolves: #9

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from posts.views import posts, post_details
+from posts.views import PostsView, post_details
 
 urlpatterns = [
-    path('', posts),
+    path('', PostsView.as_view(), name="posts"),
     path('<int:post_id>/', post_details)
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+import requests
 
 
 # POSTS VIEW ENDPOINT
-def posts(request):
-    return render(request, 'blog-listing.html')
+class PostsView(View):
+    @staticmethod
+    def get(request):
+        page = request.GET.get('page', 20)
+        response = requests.get('https://jsonplaceholder.typicode.com/posts?_page={}&_limit=5'.format(page))
+        data = response.json()
+
+        return render(request, 'blog-listing.html', {"data": data})
 
 
 # POST DETAILS VIEW ENDPOINT
 def post_details(request):
-    return render(request, 'blog-post.html')
+    return render(request, 'posts/blog-post.html')
